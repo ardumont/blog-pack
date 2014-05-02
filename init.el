79618ce1dc9e333aacb491261f3e691651898e17
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-02 10:41:54 +0200

    Refactor - Update code to more standard one (using let) + update documentation

--- init.el
+++ init.el
@@ -34,55 +34,42 @@
 
 (defun blog-pack/--setup (creds-file creds-file-content)
   "The org2blog setup (no check on the existence of the file)."
+  (message (format "'%s' found! Running org2blog setup..." creds-file))
+  (let* ((blog             (netrc-machine creds-file-content "blog" t))
+         (blog-login       (netrc-get blog "login"))
+         (blog-pass        (netrc-get blog "password")) ;; (wrap the password in " if there is space in it)
+         (blog-description (creds/get creds-file-content "blog-description"))
+         (blog-name        (creds/get-entry blog-description "blog-name"))
+         (blog-url-rpc     (creds/get-entry blog-description "blog-url-rpc")))
+    (setq org2blog/wp-blog-alist `((,blog-name
+                                    :url ,blog-url-rpc
+                                    :username ,blog-login
+                                    :password ,blog-pass)))
+    (add-hook 'org-mode-hook 'org2blog/wp-mode)
+    (add-hook 'org2blog/wp-mode-hook
+              (lambda ()
+                ;; undefine the existing binding
+                (define-key org2blog/wp-entry-mode-map (kbd "C-c p") nil)
+                (define-key org2blog/wp-entry-mode-map (kbd "C-c P") nil)
+                (define-key org2blog/wp-entry-mode-map (kbd "C-c d") nil)
+                (define-key org2blog/wp-entry-mode-map (kbd "C-c D") nil)
+                (define-key org2blog/wp-entry-mode-map (kbd "C-c t") nil)
 
-  ;; load the entry tony-blog in the ~/.netrc, we obtain a hash-map with the needed data
-  (setq blog (netrc-machine creds-file-content "blog" t))
-
-  (setq blog-login (netrc-get blog "login"))
-  (setq blog-pass (netrc-get blog "password"))
-
-  ;; blog description using creds (wrap the password in " if there is space in it)
-
-  (setq blog-description (creds/get creds-file-content "blog-description"))
-
-  ;; name of the blog
-  (setq blog-name (creds/get-entry blog-description "blog-name"))
-  (setq blog-url-rpc (creds/get-entry blog-description "blog-url-rpc"))
-
-  ;; now enter the data in the org2blog list of blogs
-  (setq org2blog/wp-blog-alist `((,blog-name
-                                  :url ,blog-url-rpc
-                                  :username ,blog-login
-                                  :password ,blog-pass)))
-
-  (add-hook 'org-mode-hook 'org2blog/wp-mode))
-
-(add-hook 'org2blog/wp-mode-hook
-          (lambda ()
-            ;; undefine the existing binding
-            (define-key org2blog/wp-entry-mode-map (kbd "C-c p") nil)
-            (define-key org2blog/wp-entry-mode-map (kbd "C-c P") nil)
-            (define-key org2blog/wp-entry-mode-map (kbd "C-c d") nil)
-            (define-key org2blog/wp-entry-mode-map (kbd "C-c D") nil)
-            (define-key org2blog/wp-entry-mode-map (kbd "C-c t") nil)
-
-            (local-set-key (kbd "C-c b p") 'org2blog/wp-post-buffer-and-publish)
-            (local-set-key (kbd "C-c b P") 'org2blog/wp-post-buffer-as-page-and-publish)
-            (local-set-key (kbd "C-c b d") 'org2blog/wp-post-buffer)
-            (local-set-key (kbd "C-c b D") 'org2blog/wp-post-buffer-as-page)
-            (local-set-key (kbd "C-c b t") 'org2blog/wp-complete-category)))
+                (local-set-key (kbd "C-c b p") 'org2blog/wp-post-buffer-and-publish)
+                (local-set-key (kbd "C-c b P") 'org2blog/wp-post-buffer-as-page-and-publish)
+                (local-set-key (kbd "C-c b d") 'org2blog/wp-post-buffer)
+                (local-set-key (kbd "C-c b D") 'org2blog/wp-post-buffer-as-page)
+                (local-set-key (kbd "C-c b t") 'org2blog/wp-complete-category)))
+    (message "Setup done!")))
 
 ;; ===================== setup routine
 
 (-if-let (creds-file-content (blog-pack/--setup-possible-p *BLOG-PACK-CREDENTIALS-FILE*))
-    (progn
-      (message (concat *BLOG-PACK-CREDENTIALS-FILE* " found! Running org2blog setup..."))
-      (blog-pack/--setup *BLOG-PACK-CREDENTIALS-FILE* creds-file-content)
-      (message "Setup done!"))
-  (progn
-    (message (concat "You need to setup the credentials file " *BLOG-PACK-CREDENTIALS-FILE* " for this to work.\n"
-                     "Here is the needed content to setup to your need into '" *BLOG-PACK-CREDENTIALS-FILE* "':\n"
-                     "machine blog login <your-wordpress-login> password <your-wordpress-password-inside-quote>\n"
-                     "machine blog-description blog-name <blog-name-you-desire> blog-url-rpc http://<path-to-your-blog-ip-and-folder>/xmlrpc.php\n"))))
+    (blog-pack/--setup *BLOG-PACK-CREDENTIALS-FILE* creds-file-content)
+  (message (concat "You need to setup the credentials file " *BLOG-PACK-CREDENTIALS-FILE* " for this to work.\n"
+                   "Here is the needed content to setup to your need into '" *BLOG-PACK-CREDENTIALS-FILE* "':\n"
+                   "machine blog login <your-wordpress-login> password <your-wordpress-password-inside-quote>\n"
+                   "machine blog-description blog-name <blog-name-you-desire> blog-url-rpc http://<path-to-your-blog-ip-and-folder>/xmlrpc.php\n"
+                   "Then `M-x encrypt-epa-file` to generate the required ~/.authinfo.gpg and remove ~/.authinfo")))
 
 ;;; blog-pack.el ends here
