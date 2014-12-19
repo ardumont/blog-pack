08081b80baff0e9cfbab1a6b9a0f942c059a8949
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-12-19 23:20:40 +0100

    Change setup from org2blog to org2jekyll

--- blog-pack.el
+++ blog-pack.el
@@ -1,80 +1,54 @@
-;;; blog-pack.el --- Blog with org-mode and wordpress + credentials setup
+;;; blog-pack.el --- Blog with org-mode and jekyll/github
 
 ;;; Commentary:
 
 ;;; Code:
 
 (require 'install-packages-pack)
-(install-packages-pack/install-packs '(;; need org2blog as a glue between org-mode and wordpress
-                                       xml-rpc
-                                       org2blog
-                                       creds
-                                       org))
-
-;; ===================== lib
+(install-packages-pack/install-packs '(org2jekyll))
 
 (require 'org)
-(require 'netrc)
-(require 'creds)
-(require 'org2blog-autoloads)
+(require 'org2jekyll)
 
-;; ===================== setup file
+(custom-set-variables '(org2jekyll/source-directory  (expand-file-name "~/org/"))
+                      '(org2jekyll/jekyll-directory  (expand-file-name "~/public_html/"))
+                      '(org2jekyll/jekyll-drafts-dir "_drafts/")
+                      '(org2jekyll/jekyll-posts-dir  "_posts/")
+                      '(org-publish-project-alist
+                        `(("tony-blog"
+                           :base-directory ,(org2jekyll/input-directory)
+                           :base-extension "org"
+                           :publishing-directory ,(org2jekyll/output-directory "_posts")
+                           :publishing-function org-html-publish-to-html
+                           :headline-levels 4
+                           :section-numbers nil
+                           :with-toc nil
+                           :html-head "<link rel=\"stylesheet\" href=\"./css/style.css\" type=\"text/css\"/>"
+                           :html-preamble t
+                           :recursive t
+                           :make-index t
+                           :html-extension "html"
+                           :body-only t)
 
-;; credentials using netrc (it can deal with space in entries)
-(defvar *BLOG-PACK-CREDENTIALS-FILE* (concat (getenv "HOME") "/.authinfo.gpg"))
+                          ("images"
+                           :base-directory ,(org2jekyll/input-directory "img")
+                           :base-extension "jpg\\|gif\\|png"
+                           :publishing-directory ,(org2jekyll/output-directory "img")
+                           :publishing-function org-publish-attachment)
 
-;; ===================== setup functions
+                          ("js"
+                           :base-directory ,(org2jekyll/input-directory "js")
+                           :base-extension "js"
+                           :publishing-directory ,(org2jekyll/output-directory "js")
+                           :publishing-function org-publish-attachment)
 
-(defun blog-pack/--setup-possible-p (creds-file)
-  "Check if the installation is possible by checking the existence of the file and that the entry 'blog' and 'blog-description' are indeed installed."
-  (let ((parsed-file (creds/read-lines creds-file)))
-    (when (and parsed-file ;; nil if the file does not exist
-               (creds/get parsed-file "blog")
-               (creds/get parsed-file "blog-description"))
-      parsed-file)))
+                          ("other"
+                           :base-directory ,(org2jekyll/input-directory "css")
+                           :base-extension "css\\|el"
+                           :publishing-directory ,(org2jekyll/output-directory "css")
+                           :publishing-function org-publish-attachment)
 
-(defun blog-pack/--setup (creds-file-content)
-  "The org2blog setup (no check on the existence of the file)."
-  (message (format "Running blog-pack setup..."))
-  (let* ((blog             (creds/get creds-file-content "blog"))
-         (blog-login       (creds/get-entry blog "login"))
-         (blog-pass        (creds/get-entry blog "password")) ;; (wrap the password in " if there is space in it)
-         (blog-description (creds/get creds-file-content "blog-description"))
-         (blog-name        (creds/get-entry blog-description "blog-name"))
-         (blog-url-rpc     (creds/get-entry blog-description "blog-url-rpc")))
-    (setq org2blog/wp-blog-alist `((,blog-name
-                                    :url ,blog-url-rpc
-                                    :username ,blog-login
-                                    :password ,blog-pass)))
-    (add-hook 'org-mode-hook 'org2blog/wp-mode)
-    (add-hook 'org2blog/wp-mode-hook
-              (lambda ()
-                ;; undefine the existing binding
-                (define-key org2blog/wp-entry-mode-map (kbd "C-c p") nil)
-                (define-key org2blog/wp-entry-mode-map (kbd "C-c P") nil)
-                (define-key org2blog/wp-entry-mode-map (kbd "C-c d") nil)
-                (define-key org2blog/wp-entry-mode-map (kbd "C-c D") nil)
-                (define-key org2blog/wp-entry-mode-map (kbd "C-c t") nil)
-
-                (local-set-key (kbd "C-c b p") 'org2blog/wp-post-buffer-and-publish)
-                (local-set-key (kbd "C-c b P") 'org2blog/wp-post-buffer-as-page-and-publish)
-                (local-set-key (kbd "C-c b d") 'org2blog/wp-post-buffer)
-                (local-set-key (kbd "C-c b D") 'org2blog/wp-post-buffer-as-page)
-                (local-set-key (kbd "C-c b t") 'org2blog/wp-complete-category)))
-    (message "Setup done!")))
-
-;; ===================== setup routine
-
-(defun blog-pack/load-pack! ()
-  (interactive)
-  "A routine to (re)load the blog-pack."
-  (-if-let (creds-file-content (blog-pack/--setup-possible-p *BLOG-PACK-CREDENTIALS-FILE*))
-      (blog-pack/--setup creds-file-content)
-    (message (concat "You need to setup the credentials file " *BLOG-PACK-CREDENTIALS-FILE* " for this to work.\n"
-                     "Here is the needed content to setup to your need into '" *BLOG-PACK-CREDENTIALS-FILE* "':\n"
-                     "machine blog login <your-wordpress-login> password <your-wordpress-password-inside-quote>\n"
-                     "machine blog-description blog-name <blog-name-you-desire> blog-url-rpc http://<path-to-your-blog-ip-and-folder>/xmlrpc.php\n"
-                     "Then `M-x encrypt-epa-file` to generate the required ~/.authinfo.gpg and remove ~/.authinfo"))))
+                          ("website" :components ("org" "tony-blog" "images" "js" "css")))))
 
 (provide 'blog-pack)
 ;;; blog-pack.el ends here
