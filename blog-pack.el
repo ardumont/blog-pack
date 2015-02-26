9ea1716becc8ecb4440f5a49de65ac38a3489c99
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-02-26 16:59:13 +0100

    Adapt my org2jekyll setup

--- blog-pack.el
+++ blog-pack.el
@@ -11,16 +11,16 @@
 (require 'org2jekyll)
 
 (custom-set-variables '(org2jekyll-blog-author       "ardumont")
-                      '(org2jekyll-source-directory  (expand-file-name "~/org/"))
-                      '(org2jekyll-jekyll-directory  (expand-file-name "~/public_html/"))
+                      '(org2jekyll-source-directory  (expand-file-name "~/org"))
+                      '(org2jekyll-jekyll-directory  (expand-file-name "~/public_html"))
                       '(org2jekyll-jekyll-drafts-dir "")
                       '(org2jekyll-jekyll-posts-dir  "_posts/")
                       '(org-publish-project-alist
                         `(("default"
-                           :base-directory ,(org2jekyll/input-directory)
+                           :base-directory ,(org2jekyll-input-directory)
                            :base-extension "org"
                            ;; :publishing-directory "/ssh:user@host:~/html/notebook/"
-                           :publishing-directory ,(org2jekyll/output-directory)
+                           :publishing-directory ,(org2jekyll-output-directory)
                            :publishing-function org-html-publish-to-html
                            :headline-levels 4
                            :section-numbers nil
@@ -33,9 +33,9 @@
                            :body-only t)
 
                           ("post"
-                           :base-directory ,(org2jekyll/input-directory)
+                           :base-directory ,(org2jekyll-input-directory)
                            :base-extension "org"
-                           :publishing-directory ,(org2jekyll/output-directory org2jekyll/jekyll-posts-dir)
+                           :publishing-directory ,(org2jekyll-output-directory org2jekyll-jekyll-posts-dir)
                            :publishing-function org-html-publish-to-html
                            :headline-levels 4
                            :section-numbers nil
@@ -48,23 +48,23 @@
                            :body-only t)
 
                           ("images"
-                           :base-directory ,(org2jekyll/input-directory "img")
+                           :base-directory ,(org2jekyll-input-directory "img")
                            :base-extension "jpg\\|gif\\|png"
-                           :publishing-directory ,(org2jekyll/output-directory "img")
+                           :publishing-directory ,(org2jekyll-output-directory "img")
                            :publishing-function org-publish-attachment
                            :recursive t)
 
                           ("js"
-                           :base-directory ,(org2jekyll/input-directory "js")
+                           :base-directory ,(org2jekyll-input-directory "js")
                            :base-extension "js"
-                           :publishing-directory ,(org2jekyll/output-directory "js")
+                           :publishing-directory ,(org2jekyll-output-directory "js")
                            :publishing-function org-publish-attachment
                            :recursive t)
 
                           ("other"
-                           :base-directory ,(org2jekyll/input-directory "css")
+                           :base-directory ,(org2jekyll-input-directory "css")
                            :base-extension "css\\|el"
-                           :publishing-directory ,(org2jekyll/output-directory "css")
+                           :publishing-directory ,(org2jekyll-output-directory "css")
                            :publishing-function org-publish-attachment
                            :recursive t)
 
